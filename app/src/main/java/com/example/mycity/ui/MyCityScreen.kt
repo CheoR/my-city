19de9714b8e57e36554723b43e2528b6509a3070
CheoR <cheo.roman+github@gmail.com>
2023-09-01 17:55:21 -0500

    layout: use material theme

--- app/src/main/java/com/example/mycity/ui/MyCityScreen.kt
+++ app/src/main/java/com/example/mycity/ui/MyCityScreen.kt
@@ -2,10 +2,11 @@ package com.example.mycity.ui
 
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,7 +22,12 @@ fun MyCityScreen (
     )
     Button(
         onClick = onNextButtonClicked,
-        modifier = modifier.widthIn(min = 250.dp)
+        modifier = modifier
+            .widthIn(min = 250.dp),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            containerColor = MaterialTheme.colorScheme.onBackground,
+        ),
     ) {
         Text("Next")// stringResource(labelResourceId))
     }
