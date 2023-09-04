6f21f4e42bd34ff12d38b56331f011d9635e3810
CheoR <cheo.roman+github@gmail.com>
2023-09-04 18:01:40 -0500

    build(gradle): material3 window size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,9 +53,10 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.05.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.06.00"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.ui:ui")
