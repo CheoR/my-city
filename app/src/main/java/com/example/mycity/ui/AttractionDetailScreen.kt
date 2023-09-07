fa1c00cbc39fafda0b610f7b46d0fe835b17a75f
CheoR <cheo.roman+github@gmail.com>
2023-09-07 11:16:57 -0500

    build: attraction detail screen

--- app/src/main/java/com/example/mycity/ui/AttractionDetailScreen.kt
+++ app/src/main/java/com/example/mycity/ui/AttractionDetailScreen.kt
@@ -6,15 +6,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.mycity.model.Category
 
 @Composable
-fun DetailScreen(
-    onNextButtonClicked: () -> Unit = {},
+fun AttractionDetailScreen(
+    attraction: Category,
+    title: String,
     modifier: Modifier = Modifier,
+    onNextButtonClicked: () -> Unit = {},
 ) {
     Text(
+        modifier = modifier,
         text = "Detail Screen!",
-        modifier = modifier
     )
     Button(
         onClick = onNextButtonClicked,
