80fe4dd7c1bceabc73b2fa4d33aa1effc242ff84
CheoR <cheo.roman+github@gmail.com>
2023-09-08 11:36:41 -0500

    chore: improve onClick parameter name

--- app/src/main/java/com/example/mycity/ui/CategoryListScreen.kt
+++ app/src/main/java/com/example/mycity/ui/CategoryListScreen.kt
@@ -1,6 +1,5 @@
 package com.example.mycity.ui
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.mycity.model.Category
@@ -8,28 +7,14 @@ import com.example.mycity.model.Category
 @Composable
 fun CategoryListScreen (
     categories: List<Category>,
-    title: String,
 //    onNextButtonClicked: (Int) -> Unit,
-    onClick: (Category) -> Unit,
+    onCategoryClick: (Any) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        text = "Hello $title! Go eat!",
-        modifier = modifier
-    )
+
     BaseListScreen(
+        modifier = modifier,
         list = categories,
-        onClick = onClick,
+        onClick = onCategoryClick,
     )
-//        Button(
-//            onClick = onNextButtonClicked,
-//            modifier = modifier
-//                .widthIn(min = 250.dp),
-//            colors = ButtonDefaults.buttonColors(
-//                contentColor = MaterialTheme.colorScheme.onPrimary,
-//                containerColor = MaterialTheme.colorScheme.onBackground,
-//            ),
-//        ) {
-//            Text("Next")// stringResource(labelResourceId))
-//        }
 }
