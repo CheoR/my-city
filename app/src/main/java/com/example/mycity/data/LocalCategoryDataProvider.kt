df76a7260b43f3d190150de0fcb686c06077e56f
CheoR <cheo.roman+github@gmail.com>
2023-09-08 11:34:08 -0500

    build: category data provider category type field for easier filtering

--- app/src/main/java/com/example/mycity/data/LocalCategoryDataProvider.kt
+++ app/src/main/java/com/example/mycity/data/LocalCategoryDataProvider.kt
@@ -2,6 +2,7 @@ package com.example.mycity.data
 
 import com.example.mycity.R
 import com.example.mycity.model.Category
+import com.example.mycity.model.CategoryType
 
 object LocalCategoryDataProvider {
     val defaultCategory = getCategoryData()[0]
@@ -11,25 +12,30 @@ object LocalCategoryDataProvider {
             Category(
                 id =  1,
                 categoryResourceId = R.string.music,
+                categoryType = CategoryType.MUSIC,
                 imageResourceId = R.drawable.placeholder,
             ),
             Category(
                 id =  2,
                 categoryResourceId = R.string.food,
+                categoryType = CategoryType.FOOD,
                 imageResourceId = R.drawable.placeholder,
             ),
             Category(
                 id =  3,
+                categoryType = CategoryType.HISTORICAL_AND_CULTURE,
                 categoryResourceId = R.string.historical_and_cultural,
                 imageResourceId = R.drawable.placeholder,
             ),
             Category(
                 id =  4,
+                categoryType = CategoryType.OUTDOORS_AND_NATURE,
                 categoryResourceId = R.string.outdoor_and_nature,
                 imageResourceId = R.drawable.placeholder,
             ),
             Category(
                 id =  5,
+                categoryType = CategoryType.NIGHTLIFE_AND_ENTERTAINMENT,
                 categoryResourceId = R.string.nightlife_and_entertainment,
                 imageResourceId = R.drawable.placeholder,
             ),
--- app/src/main/java/com/example/mycity/model/Category.kt
+++ app/src/main/java/com/example/mycity/model/Category.kt
@@ -3,8 +3,17 @@ package com.example.mycity.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
+enum class CategoryType {
+    MUSIC,
+    FOOD,
+    HISTORICAL_AND_CULTURE,
+    OUTDOORS_AND_NATURE,
+    NIGHTLIFE_AND_ENTERTAINMENT,
+}
+
 data class Category(
     val id: Int,
+    val categoryType: CategoryType,
     @StringRes val categoryResourceId: Int,
     @DrawableRes val imageResourceId: Int,
 )
