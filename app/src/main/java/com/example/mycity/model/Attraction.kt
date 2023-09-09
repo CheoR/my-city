217025a2d7f64079f96edc49bc84a58c3fb71206
CheoR <cheo.roman+github@gmail.com>
2023-09-09 10:29:17 -0500

    chore: attraction detail string

--- app/src/main/java/com/example/mycity/model/Attraction.kt
+++ app/src/main/java/com/example/mycity/model/Attraction.kt
@@ -7,5 +7,6 @@ data class Attraction(
     val id: Int,
     val category: CategoryType,
     @StringRes val attractionResourceId: Int,
+    @StringRes val attractionDetailId: Int,
     @DrawableRes val imageResourceId: Int,
 )
